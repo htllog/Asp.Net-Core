5c5beb935b6b8c18f0fa440830c56b35d27b2865
Bandi.Z <htllog@163.com>
2023-10-03 09:02:50 +0800

    Initialize project

---
+++ WebCore/WebCore/Program.cs
@@ -0,0 +1,16 @@
+using WebCore;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuildr =>
+            {
+                webBuildr.UseStartup<Startup>();
+            });
+}
